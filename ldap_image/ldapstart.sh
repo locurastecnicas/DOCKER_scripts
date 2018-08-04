26f9fc705d7141e172e7d83fe56f7fa706c0e138
locurastecnicas@gmail.com <locurastecnicas@gmail.com>
2018-08-04 12:44:20 +0200

    Comprobacion basica y sustitucion de variables requeridas.

--- ldap_image/ldapstart.sh
+++ ldap_image/ldapstart.sh
@@ -7,18 +7,22 @@
 ##   LDAPsuffix
 ##   LDAProotdn
 ##   LDAProotpw
-##   LDAPdirectory
 ##  OPCIONALES
 ##   LDAPschema
 ##   LDAPmaxsize
+##   LDAPdirectory
 ##   LDAPindex
 
 ## Comprobacion de variables requeridas.
-if [ -z "${LDAPsuffix}" ] || [ -z "${LDAProotdn} ] || [ -z "${LDAProotpw}" ] || [ -z "${LDAPdirectory}" ]
+if [ -z "${LDAPsuffix}" ] || [ -z "${LDAProotdn}" ] || [ -z "${LDAProotpw}" ]
 then
   /bin/echo "Error, falta una de las variables requeridas de configuracion."
-  /bin/echo "Las variables requeridas son: LDAPsuffix, LDAProotdn, LDAProotpw, LDAPdirectory."
+  /bin/echo "Las variables requeridas son: LDAPsuffix, LDAProotdn, LDAProotpw."
   exit 1
 fi
 
+## Sustitucion en el fichero slapd.conf.PRE de las variables inyectadas en el contendor.
+/usr/bin/sed -e s/LDAPsuffix/${LDAPsuffix}/ \
+             -e s/LDAProotdn/${LDAProotdn}/ \
+             -e s/LDAProotpw/${LDAProotpw}/ ./slapd.conf.PRE
 
