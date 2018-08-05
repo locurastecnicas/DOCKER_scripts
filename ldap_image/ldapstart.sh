a13cae4d460fd4bfb1bed46bd5cbc6faf75512a7
locurastecnicas@gmail.com <locurastecnicas@gmail.com>
2018-08-05 11:18:32 +0200

    Comprobacion y sustitucion de variables completado.

--- ldap_image/ldapstart.sh
+++ ldap_image/ldapstart.sh
@@ -8,12 +8,19 @@
 ##   LDAProotdn
 ##   LDAProotpw
 ##  OPCIONALES
-##   LDAPschema
+##   LDAPschema - Formato de entrada: "RUTA_FICHERO1.schema|RUTA_FICHERO2.schema|..."
 ##   LDAPmaxsize
 ##   LDAPdirectory
 ##   LDAPindex - Formato de entrada: "NOMBRE|TipoIndice1,TipoIndice2,...:NOMBRE|TipoIndice1,TipoIndice2,...:..."
 
 ## Valores por defecto para variables opcionales.
+if [ -z "${LDAPschema}" ]
+then
+  LDAPschema="include /ldap/etc/openldap/schema/core.schema"
+else
+  LDAPschema=`/usr/bin/echo -e "include\t${LDAPschema}" | /usr/bin/sed 's/|/\ninclude\t/g'`
+fi
+
 if [ -z "${LDAPmaxsize}" ]
 then
   LDAPmaxsize=1073741824
@@ -37,6 +44,11 @@ then
   exit 1
 fi
 
+## Creacion del fichero de configuracion con la especificacion de schema.
+/bin/echo "# Ficheros de schema." > ./slapd.conf
+/bin/echo -e "${LDAPschema}" >> ./slapd.conf
+/bin/echo ""  >> ./slapd.conf
+
 ## Sustitucion en el fichero slapd.conf.PRE de las variables inyectadas en el contendor.
 /usr/bin/sed -e s/LDAPsuffix/${LDAPsuffix}/ \
              -e s/LDAProotdn/${LDAProotdn}/ \
@@ -44,4 +56,6 @@ fi
              -e s/LDAPmaxsize/${LDAPmaxsize}/ \
              -e 's#LDAPdirectory#'${LDAPdirectory}'#' ./slapd.conf.PRE >> ./slapd.conf
 
+## Finalizacion del archivo con la definicion de indices.
 /bin/echo -e "index\t${LDAPindex}" >> ./slapd.conf
+
