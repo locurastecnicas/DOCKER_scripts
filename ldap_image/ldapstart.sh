d0b35326117a29a87b090accedcab2e1a608c24d
locurastecnicas@gmail.com <locurastecnicas@gmail.com>
2018-08-05 09:21:49 +0200

    Control y sustitucion de variables requeridas terminado.

--- ldap_image/ldapstart.sh
+++ ldap_image/ldapstart.sh
@@ -13,6 +13,16 @@
 ##   LDAPdirectory
 ##   LDAPindex
 
+## Valores por defecto para variables opcionales.
+if [ -z "${LDAPmaxsize}" ]
+then
+  LDAPmaxsize=1073741824
+fi
+if [ -z "${LDAPdirectory}" ]
+then
+  LDAPdirectory="/ldap/var/openldap-data"
+fi
+
 ## Comprobacion de variables requeridas.
 if [ -z "${LDAPsuffix}" ] || [ -z "${LDAProotdn}" ] || [ -z "${LDAProotpw}" ]
 then
@@ -24,5 +34,6 @@ fi
 ## Sustitucion en el fichero slapd.conf.PRE de las variables inyectadas en el contendor.
 /usr/bin/sed -e s/LDAPsuffix/${LDAPsuffix}/ \
              -e s/LDAProotdn/${LDAProotdn}/ \
-             -e s/LDAProotpw/${LDAProotpw}/ ./slapd.conf.PRE
-
+             -e s/LDAProotpw/${LDAProotpw}/ \
+             -e s/LDAPmaxsize/${LDAPmaxsize}/ \
+             -e 's#LDAPdirectory#'${LDAPdirectory}'#' ./slapd.conf.PRE
